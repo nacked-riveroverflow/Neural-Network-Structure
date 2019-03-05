4c3a36292f7db9fc19901ebd761754e722023162
Winston Zhu <Zhu_Hertz@hotmail.com>
2019-03-05 00:10:57 -0500

    Add files via upload

---
+++ h86zhu_a2.py
@@ -0,0 +1,437 @@
+import numpy as np
+import matplotlib.pylab as plt
+get_ipython().run_line_magic('matplotlib', 'inline')
+import importlib
+import time
+
+def NSamples(x):
+    '''
+        n = NSamples(x)
+        
+        Returns the number of samples in a batch of inputs.
+        
+        Input:
+         x   is a 2D array
+        
+        Output:
+         n   is an integer
+    '''
+    return len(x)
+
+def Shuffle(inputs, targets):
+    '''
+        s_inputs, s_targets = Shuffle(inputs, targets)
+        
+        Randomly shuffles the dataset.
+        
+        Inputs:
+         inputs     array of inputs
+         targets    array of corresponding targets
+         
+        Outputs:
+         s_inputs   shuffled array of inputs
+         s_targets  corresponding shuffled array of targets
+    '''
+    data = list(zip(inputs,targets))
+    np.random.shuffle(data)
+    s_inputs, s_targets = zip(*data)
+    return np.array(s_inputs), np.array(s_targets)
+
+def Logistic(z):
+    '''
+        y = Logistic(z)
+
+        Applies the logistic function to each element in z.
+
+        Input:
+         z    is a scalar, list or array
+
+        Output:
+         y    is the same shape as z
+    '''
+    return 1. / (1 + np.exp(-z) )
+
+def Logistic_p(h):
+    '''
+        yp = Logistic_p(h)
+        
+        Returns the slope of the logistic function at z when h = Logistic(z).
+        Note the h is the input, NOT z.
+    '''
+    return h*(1.-h)
+
+def Identity(z):
+    '''
+        y = Identity(z)
+
+        Does nothing... simply returns z.
+
+        Input:
+         z    is a scalar, list or array
+
+        Output:
+         y    is the same shape as z
+    '''
+    return z
+
+def Identity_p(h):
+    '''
+        yp = Identity_p(h)
+        
+        Returns the slope of the identity function h.
+    '''
+    return np.ones_like(h)
+
+def OneHot(z):
+    '''
+        y = OneHot(z)
+
+        Applies the one-hot function to the vectors in z.
+        Example:
+          OneHot([[0.9, 0.1], [-0.5, 0.1]])
+          returns np.array([[1,0],[0,1]])
+
+        Input:
+         z    is a 2D array of samples
+
+        Output:
+         y    is an array the same shape as z
+    '''
+    y = []
+    # Locate the max of each row
+    for zz in z:
+        idx = np.argmax(zz)
+        b = np.zeros_like(zz)
+        b[idx] = 1.
+        y.append(b)
+    y = np.array(y)
+    return y
+
+
+# ## Layer Class
+
+# In[4]:
+
+
+class Layer():
+    
+    def __init__(self, n_nodes, act='logistic'):
+        '''
+            lyr = Layer(n_nodes, act='logistic')
+            
+            Creates a layer object.
+            
+            Inputs:
+             n_nodes  the number of nodes in the layer
+             act      specifies the activation function
+                      Use 'logistic' or 'identity'
+        '''
+        self.N = n_nodes  # number of nodes in this layer
+        self.h = []       # node activities
+        self.b = np.zeros(self.N)  # biases
+        
+        # Activation functions
+        self.sigma = Logistic
+        self.sigma_p = (lambda : Logistic_p(self.h))
+        if act=='identity':
+            self.sigma = Identity
+            self.sigma_p = (lambda : Identity_p(self.h))
+
+
+# ## Network Class
+
+# In[5]:
+
+
+class Network():
+
+    def __init__(self, sizes, type='classifier'):
+        '''
+            net = Network(sizes, type='classifier')
+
+            Creates a Network and saves it in the variable 'net'.
+
+            Inputs:
+              sizes is a list of integers specifying the number
+                  of nodes in each layer
+                  eg. [5, 20, 3] will create a 3-layer network
+                      with 5 input, 20 hidden, and 3 output nodes
+              type can be either 'classifier' or 'regression', and
+                  sets the activation function on the output layer,
+                  as well as the loss function.
+                  'classifier': logistic, cross entropy
+                  'regression': linear, mean squared error
+        '''
+        self.n_layers = len(sizes)
+        self.lyr = []    # a list of Layers
+        self.W = []      # Weight matrices, indexed by the layer below it
+        
+        # Two common types of networks
+        # The member variable self.Loss refers to one of the implemented
+        # loss functions: MSE, or CrossEntropy.
+        # Call it using self.Loss(t)
+        if type=='classifier':
+            self.classifier = True
+            self.Loss = self.CrossEntropy
+            activation = 'logistic'
+        else:
+            self.classifier = False
+            self.Loss = self.MSE
+            activation = 'identity'
+
+        # Create and add Layers (using logistic for hidden layers)
+        for n in sizes[:-1]:
+            self.lyr.append( Layer(n) )
+   
+        # For the top layer, we use the appropriate activtaion function
+        self.lyr.append( Layer(sizes[-1], act=activation) )
+    
+        # Randomly initialize weight matrices
+        for idx in range(self.n_layers-1):
+            m = self.lyr[idx].N
+            n = self.lyr[idx+1].N
+            temp = np.random.normal(size=[m,n])/np.sqrt(m)
+            self.W.append(temp)
+
+
+    def FeedForward(self, x):
+        '''
+            y = net.FeedForward(x)
+
+            Runs the network forward, starting with x as input.
+            Returns the activity of the output layer.
+        '''
+        x = np.asarray(x)  # Convert input to array, in case it's not
+        self.lyr[0].h = x # Define first activity is just input
+        for i in range(self.n_layers - 1):
+            next_input_cur = np.asarray(np.asarray((self.lyr[i]).h) @ (self.W[i]) + np.asarray(self.lyr[i+1].b))
+                    #Input Current for next Layer
+            self.lyr[i+1].h = np.array(self.lyr[i+1].sigma(next_input_cur)); 
+                    # apply activation function to input current
+        
+        
+        #===== YOUR CODE HERE =====
+        
+        return self.lyr[-1].h
+
+    
+    def Evaluate(self, inputs, targets):
+        '''
+            E = net.Evaluate(data)
+
+            Computes the average loss over the supplied dataset.
+
+            Inputs
+             inputs  is an array of inputs
+             targets is a list of corresponding targets
+
+            Outputs
+             E is a scalar, the average loss
+        '''
+        y = self.FeedForward(inputs)
+        return self.Loss(targets)
+
+    def ClassificationAccuracy(self, inputs, targets):
+        '''
+            a = net.ClassificationAccuracy(data)
+            
+            Returns the fraction (between 0 and 1) of correct one-hot classifications
+            in the dataset.
+        '''
+        y = self.FeedForward(inputs)
+        yb = OneHot(y)
+        n_incorrect = np.sum(yb!=targets) / 2.
+        return 1. - float(n_incorrect) / NSamples(inputs)
+
+    
+    def CrossEntropy(self, t):
+        '''
+            E = net.CrossEntropy(t)
+
+            Evaluates the mean cross entropy loss between t and the activity of the top layer.
+            To evaluate the network's performance on an input/output pair (x,t), use
+              net.FeedForward(x)
+              E = net.Loss(t)
+
+            Inputs:
+              t is an array holding the target output
+
+            Outputs:
+              E is the loss function for the given case
+        '''
+        
+        #===== YOUR CODE HERE =====
+        last_layer_h = self.lyr[-1].h
+        E = -np.sum(t * np.log(last_layer_h) + (1 - t) * np.log(1 - last_layer_h))/len(t);
+        
+        return E
+
+    
+    def MSE(self, t):
+        '''
+            E = net.MSE(t)
+
+            Evaluates the MSE loss function using t and the activity of the top layer.
+            To evaluate the network's performance on an input/output pair (x,t), use
+              net.FeedForward(x)
+              E = net.Loss(t)
+
+            Inputs:
+              t is an array holding the target output
+
+            Outputs:
+              E is the loss function for the given case
+        '''
+        
+        #===== YOUR CODE HERE =====
+        last_layer_h = self.lyr[-1].h;
+        return np.sum((last_layer_h - t)**2)/len(t);
+        
+        
+        return 0.
+
+    
+    def BackProp(self, t, lrate=0.05):
+        '''
+            net.BackProp(targets, lrate=0.05)
+            
+            Given the current network state and targets t, updates the connection
+            weights and biases using the backpropagation algorithm.
+            
+            Inputs:
+             t      an array of targets (number of samples must match the
+                    network's output)
+             lrate  learning rate
+        '''
+        t = np.array(t)  # convert t to an array, in case it's not
+        
+        #===== YOUR CODE HERE =====
+        dEdz = 2 * (self.lyr[-1].h - t)/ len(t);
+        #print(np.shape(dEdz));
+        for i in range(self.n_layers - 2, -1, -1):
+            back = self.lyr[i];
+            dEdb = np.sum(dEdz,axis = 0)
+            dEdw = (back.h.T) @ dEdz;
+            #print(np.shape(dEdz))
+            #dEdz = back.sigma_p() @ ((self.W[i].T) * dEdz);
+            dEdz = dEdz @ (self.W[i].T) * back.sigma_p();
+            self.W[i] = self.W[i] - lrate * dEdw;
+            # E(y,t) = E(sigma*(M*layer[i-1].h + b), t)
+            self.lyr[i+1].b -= lrate*dEdb
+        
+        
+        
+
+    def Learn(self, inputs, targets, lrate=0.05, epochs=1):
+        '''
+            Network.Learn(inputs, targets, lrate=0.05, epochs=1)
+
+            Run through the dataset 'epochs' number of times, incrementing the
+            network weights for each training sample. For each epoch, it
+            shuffles the order of the samples.
+
+            Inputs:
+              inputs  is an array of input samples
+              targets is a corresponding array of targets
+              lrate   is the learning rate (try 0.001 to 0.5)
+              epochs  is the number of times to go through the training data
+        '''
+        
+        #===== YOUR CODE HERE =====
+        for i in range(epochs):
+            shuffled_input, shuffled_target = Shuffle(inputs, targets)
+            self.FeedForward(shuffled_input);
+            self.BackProp(shuffled_target);
+        
+
+
+# # Classification
+
+# ## Create a Classification Dataset
+
+# 5 Classes in 8-Dimensional Space
+np.random.seed(15)
+noise = 0.1
+InputClasses = np.array([[1,0,1,0,0,1,1,0],
+                         [0,1,0,1,0,1,0,1],
+                         [0,1,1,0,1,0,0,1],
+                         [1,0,0,0,1,0,1,1],
+                         [1,0,0,1,0,1,0,1]], dtype=float)
+OutputClasses = np.array([[1,0,0,0,0],
+                          [0,1,0,0,0],
+                          [0,0,1,0,0],
+                          [0,0,0,1,0],
+                          [0,0,0,0,1]], dtype=float)
+n_input = np.shape(InputClasses)[1]
+n_output = np.shape(OutputClasses)[1]
+n_classes = np.shape(InputClasses)[0]
+
+# Create a training dataset
+n_samples = 100
+training_output = []
+training_input = []
+for idx in range(n_samples):
+    k = np.random.randint(n_classes)
+    x = InputClasses[k,:] + np.random.normal(size=n_input)*noise
+    t = OutputClasses[k,:]
+    training_input.append(x)
+    training_output.append(t)
+
+# Create a test dataset
+n_samples = 100
+test_output = []
+test_input = []
+for idx in range(n_samples):
+    k = np.random.randint(n_classes)
+    x = InputClasses[k,:] + np.random.normal(size=n_input)*noise
+    t = OutputClasses[k,:]
+    test_input.append(x)
+    test_output.append(t)
+
+train = [np.array(training_input), np.array(training_output)]
+test = [np.array(test_input), np.array(test_output)]
+
+
+# ## Neural Network Model
+
+# In[7]:
+
+
+# Create a Network
+net = Network([n_input, 18, n_output], type='classifier')
+
+
+# In[8]:
+
+
+# Evaluate it before training
+CE = net.Evaluate(train[0], train[1])
+accuracy = net.ClassificationAccuracy(train[0], train[1])
+print('Cross Entropy = '+str(CE))
+print('     Accuracy = '+str(accuracy*100.)+'%')
+
+
+# In[9]:
+
+
+net.Learn(train[0], train[1], epochs=500, lrate=1.)
+
+
+# ## Evaluate it After Training
+
+
+print('Training Set')
+CE = net.Evaluate(train[0], train[1])
+accuracy = net.ClassificationAccuracy(train[0], train[1])
+print('Cross Entropy = '+str(CE))
+print('     Accuracy = '+str(accuracy*100.)+'%')
+
+
+
+
+print('Test Set')
+CE = net.Evaluate(test[0], test[1])
+accuracy = net.ClassificationAccuracy(test[0], test[1])
+print('Cross Entropy = '+str(CE))
+print('     Accuracy = '+str(accuracy*100.)+'%')
+
